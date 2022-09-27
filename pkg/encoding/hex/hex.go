42df79229ce23f954d232aa148b674d6a8bdf485
Und3rf10w <jrec93+github@gmail.com>
2022-09-27 04:16:08 -0400

    lol 0 -> -1 in hex encoder

--- pkg/encoding/hex/hex.go
+++ pkg/encoding/hex/hex.go
@@ -4,20 +4,20 @@ import "bytes"
 
 // Encode a given byte array using a 'custom' hex encoder
 func Encode(message []byte) []byte {
-	message = bytes.Replace(message, []byte("a"), []byte("j"), 0)
-	message = bytes.Replace(message, []byte("c"), []byte("n"), 0)
-	message = bytes.Replace(message, []byte("e"), []byte("l"), 0)
-	message = bytes.Replace(message, []byte("f"), []byte("g"), 0)
+	message = bytes.Replace(message, []byte("a"), []byte("j"), -1)
+	message = bytes.Replace(message, []byte("c"), []byte("n"), -1)
+	message = bytes.Replace(message, []byte("e"), []byte("l"), -1)
+	message = bytes.Replace(message, []byte("f"), []byte("g"), -1)
 
 	return message
 }
 
 // Decode a given byte array using a 'custom' hex encoder
 func Decode(message []byte) []byte {
-	message = bytes.Replace(message, []byte("j"), []byte("a"), 0)
-	message = bytes.Replace(message, []byte("n"), []byte("c"), 0)
-	message = bytes.Replace(message, []byte("l"), []byte("e"), 0)
-	message = bytes.Replace(message, []byte("g"), []byte("f"), 0)
+	message = bytes.Replace(message, []byte("j"), []byte("a"), -1)
+	message = bytes.Replace(message, []byte("n"), []byte("c"), -1)
+	message = bytes.Replace(message, []byte("l"), []byte("e"), -1)
+	message = bytes.Replace(message, []byte("g"), []byte("f"), -1)
 
 	return message
 }
