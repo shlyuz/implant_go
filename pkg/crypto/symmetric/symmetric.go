415c24704c53f8c97e9a34300e8852de14514366
Und3rf10w <jrec93+github@gmail.com>
2022-09-27 16:15:01 -0400

    trying to fix chunking for symmetric.encrypt

--- pkg/crypto/symmetric/symmetric.go
+++ pkg/crypto/symmetric/symmetric.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	rand "crypto/rand"
 
-	"shlyuz/pkg/crypto/rc6"
+	rc6 "shlyuz/pkg/crypto/rc6"
 )
 
 type SymmetricMessage struct {
@@ -25,7 +25,7 @@ func generateKey() []byte {
 //	This assumes the message is perfect. It will trigger a crash if it recieves invalid data
 func pad(message []byte) []byte {
 	// TODO: Handle invalid data here
-	n := 16 - ((len(message)) % 16)
+	n := 16 - (len(message) % 16)
 	paddedBytes := make([]byte, len(message)+n)
 	copy(paddedBytes, message)
 	copy(paddedBytes[len(message):], bytes.Repeat([]byte{byte(n)}, n))
@@ -38,6 +38,14 @@ func pad(message []byte) []byte {
 func unpad(message []byte) []byte {
 	size := message[len(message)-1]
 	x := int(size)
+	if x == 0 || x > 16 {
+		return message
+	}
+	// for i := 0; i < x; i++ {
+	// 	if message[len(message)-x+i] != size {
+	// 		return message
+	// 	}
+	// }
 	// TODO: handle invalid padding here
 	return message[:len(message)-x]
 }
@@ -59,15 +67,26 @@ func Encrypt(plaintext []byte) SymmetricMessage {
 	EncryptedMessage.Key = generateKey()
 
 	cipher := rc6.NewCipher(EncryptedMessage.Key)
+
 	// Chunking logic, appends to EncryptedMessage.Message every 16 bytes
 	//   APPEND to EncryptedMessage.Message since this library won't loop for you. Loop across every 16 bytes of paddedPlaintext (paddedChunk)
 	for i := 0; i < len(paddedPlaintext); i = i + 16 {
-		j := i + 16
-		paddedChunk = paddedPlaintext[i:j]
+		j := 0
+		if i > len(paddedPlaintext) {
+			j = i - len(paddedPlaintext)
+		} else {
+			j = 0
+		}
+		if (len(paddedPlaintext) != 16 || len(paddedPlaintext) > 16) && i > len(paddedPlaintext) {
+			j = j + 16
+			paddedChunk = paddedPlaintext[i-16 : j+(len(paddedPlaintext)-i)]
+		} else {
+			j = j + 16
+			paddedChunk = paddedPlaintext[i:j]
+		}
 		cipher.Encrypt(paddedChunk, paddedPlaintext[i:j])
 		EncryptedMessage.Message = append(EncryptedMessage.Message[:], paddedChunk[:]...)
 	}
-
 	EncryptedMessage.IsEncrypted = true
 
 	return EncryptedMessage
@@ -98,7 +117,11 @@ func Decrypt(encryptedText []byte, decryptionKey []byte) SymmetricMessage {
 		cipher.Decrypt(unpaddedChunk, unpaddedText[i:j])
 		DecryptedMessage.Message = append(DecryptedMessage.Message[:], unpaddedChunk[:]...)
 	}
+	// if (len(DecryptedMessage.Message) < 16) || (len(DecryptedMessage.Message)%4 != 0) {
+	// 	DecryptedMessage.Message = unpad(DecryptedMessage.Message)
+	// }
 	//   APPEND to DecryptedMessage.Message since this library won't loop for you. You need to loop across every 16 bytes of plaintext
+	DecryptedMessage.Message = unpad(DecryptedMessage.Message)
 	DecryptedMessage.IsEncrypted = false
 
 	return DecryptedMessage
